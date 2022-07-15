010632211da109601e70399e26736960b48c7f0e
Rodrigo Moutinho <78484497+rodrigomoutinho@users.noreply.github.com>
2022-07-15 19:25:41 -0300

    Update beauty.sql

--- SQL File/beauty.sql
+++ SQL File/beauty.sql
@@ -12,15 +12,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `beauty`
---
-
 -- --------------------------------------------------------
 
 --
@@ -47,13 +38,6 @@ CREATE TABLE `tblappointment` (
 --
 
 INSERT INTO `tblappointment` (`ID`, `AptNumber`, `Name`, `Email`, `PhoneNumber`, `AptDate`, `AptTime`, `Services`, `ApplyDate`, `Remark`, `Status`, `RemarkDate`) VALUES
-(2, '985645887', 'Sulaita Vernela', 'vernela@gmail.com', 4654654654, '7/29/2021', '4:30pm', 'Deluxe Pedicure', '2021-07-26 05:04:38', 'Rejected', '2', '2021-07-26 06:47:04'),
-(3, '965887988', 'Jain Gloria', 'gloria@gmail.com', 5646464646, '6/20/2021', '2:30pm', 'Loreal Hair Color(Full)', '2021-06-19 12:35:30', 'we will wait', '1', '2021-06-19 13:37:39'),
-(6, '621107928', 'Tracy Peace', 'peace@gmail.com', 1234567890, '6/27/2021', '1:30am', 'Rebonding', '2021-06-21 16:22:25', 'Testing', '2', '2021-06-21 16:24:10'),
-(7, '252539813', 'Don Williams', 'williams@gmail.com', 770546590, '06/18/2021', '3:00pm', 'Layer Haircut', '2021-06-12 10:25:50', 'well accepted', '1', '2021-07-24 12:18:39'),
-(8, '402172811', 'Tracy Peace', 'peace@gmail.com', 770546590, '11/06/2021', '12:30am', 'U-Shape Hair Cut', '2021-06-18 07:18:00', '', '', '2021-07-09 15:56:26'),
-(10, '747579846', 'Don Williams', 'williams@gmail.com', 770546590, '7/5/2021', '12:30am', 'Deluxe Menicure', '2021-07-02 22:16:42', '', '', '2021-07-09 15:52:55'),
-(11, '924501752', 'Sulaita Vernela', 'vernela@gmail.com', 770546590, '7/7/2021', '12:30am', 'U-Shape Hair Cut', '2021-07-02 22:45:27', '', '', '2021-07-09 15:59:25');
 
 -- --------------------------------------------------------
 
@@ -77,10 +61,6 @@ CREATE TABLE `tblcustomers` (
 --
 
 INSERT INTO `tblcustomers` (`ID`, `Name`, `Email`, `MobileNumber`, `Gender`, `Details`, `CreationDate`, `UpdationDate`) VALUES
-(1, 'Sulaita Vernela', 'vernela@gmail.com', 5546464646, 'Male', 'From California', '2019-07-26 11:09:10', '2021-07-09 15:10:37'),
-(2, 'Don Williams', 'williams@gmail.com', 5565565656, 'Male', 'from Canada', '2019-07-26 11:10:02', '2021-07-09 15:11:10'),
-(3, 'Tracy Peace', 'peace@gmail.com', 789465990, 'Female', 'Taking massage', '2019-07-26 11:10:28', '2021-07-09 15:35:28'),
-(4, ' Jain Gloria', 'gloria@gmail.com', 5646464646, 'Female', 'from California', '2019-08-19 13:38:58', '2021-07-09 15:12:19');
 
 -- --------------------------------------------------------
 
@@ -101,17 +81,6 @@ CREATE TABLE `tblinvoice` (
 --
 
 INSERT INTO `tblinvoice` (`id`, `Userid`, `ServiceId`, `BillingId`, `PostingDate`) VALUES
-(26, 3, 2, 379605551, '2021-07-09 15:32:37'),
-(27, 3, 4, 379605551, '2021-07-09 15:32:37'),
-(28, 3, 9, 379605551, '2021-07-09 15:32:37'),
-(33, 2, 8, 107583558, '2021-07-09 15:42:57'),
-(34, 2, 12, 107583558, '2021-07-09 15:42:57'),
-(35, 1, 1, 372872256, '2021-07-09 15:43:52'),
-(36, 1, 2, 372872256, '2021-07-09 15:43:52'),
-(37, 1, 4, 372872256, '2021-07-09 15:43:52'),
-(38, 2, 1, 361165436, '2021-07-24 11:52:27'),
-(39, 2, 2, 361165436, '2021-07-24 11:52:27'),
-(40, 2, 3, 361165436, '2021-07-24 11:52:27');
 
 -- --------------------------------------------------------
 
@@ -165,7 +134,6 @@ CREATE TABLE `tblusers` (
 
 INSERT INTO `tblusers` (`id`, `name`, `lastname`, `username`, `email`, `sex`, `permission`, `password`, `mobile`, `userimage`, `status`) VALUES
 (15, 'Rodrigo', 'Moutinho ', 'admin', 'rodrigomoutinhoadv@outlook.com', 'Male', 'Super User', '123456', 770546590, 'face19.jpg', 1),
-(21, 'Arinaitwe', 'Gerald', 'gerald', 'gerald@gmail.com', 'Male', 'Admin', '81dc9bdb52d04dc20036dbd8313ed055', 770546590, 'but.jpg', 0);
 
 --
 -- Indexes for dumped tables
@@ -237,6 +205,3 @@ ALTER TABLE `tblusers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
